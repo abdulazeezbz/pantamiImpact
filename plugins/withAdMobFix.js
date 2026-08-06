81fdd2273ca6a48d0540e94ca3c2992d73c4d0dd
AbdulAzeez Bello <abdulazeezbello5882@gmail.com>
2026-08-06 22:59:15 +0100

    Force play-services-ads to 25.3.0

--- plugins/withAdMobFix.js
+++ plugins/withAdMobFix.js
@@ -1,17 +1,17 @@
 const { withProjectBuildGradle } = require('@expo/config-plugins');
 
 /**
- * Expo Config Plugin to force play-services-ads version to 24.3.0.
- * This satisfies react-native-google-mobile-ads 16.4.0 (requires AgeRestrictedTreatment API)
- * while avoiding the Kotlin 2.3.0 metadata incompatibility bug introduced in play-services-ads 25.4.0.
+ * Expo Config Plugin to force play-services-ads version to 25.3.0.
+ * This satisfies react-native-google-mobile-ads 16.4.0 (requires AgeRestrictedTreatment API in v25)
+ * while avoiding the Kotlin 2.3.0 metadata incompatibility introduced in play-services-ads 25.4.0.
  */
 module.exports = function withAdMobFix(config) {
   return withProjectBuildGradle(config, (config) => {
-    if (!config.modResults.contents.includes('play-services-ads:24.3.0')) {
+    if (!config.modResults.contents.includes('play-services-ads:25.3.0')) {
       const resolutionSnippet = `
   configurations.all {
     resolutionStrategy {
-      force 'com.google.android.gms:play-services-ads:24.3.0'
+      force 'com.google.android.gms:play-services-ads:25.3.0'
     }
   }`;
 
